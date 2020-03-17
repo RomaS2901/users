e477de8a7854e051844bf0169e5ca314d2e1f87e
Роман Бондаренко <roman.bondarenko@nikel.com.ua>
2020-03-17 21:06:55 +0200

    Added MySql config database in settings

--- users_project/settings.py
+++ users_project/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'users_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testDatabase',
+        'USER': 'root',
+        'PASSWORD': 'tre12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
